73b63943c84ac8bd00f6a40e766698ecac685e03
michaelpkuhn <rakuhn14@gmail.com>
2020-03-28 12:50:10 -0500

    flask app setup

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -11,14 +11,6 @@ from flask import Flask, jsonify
 #################################################
 engine = create_engine("sqlite:///../data/chiTransport2.sqlite")
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
-
-# Save reference to the table
-scoot = Base.classes.scoot
-
 #################################################
 # Flask Setup
 #################################################
@@ -27,4 +19,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return(scoot)
+    #Scoot JSON
+    scootResults = engine.execute("Select * from randomScoot WHERE [Start Census Tract] != '' AND [End Community Area Number] != '' AND [Trip Distance] != '0' LIMIT 10000").fetchall()
+    scootJson = []
+    #Start and End Time rounded to the nearest hour
+    for result in scootResults:
+        r = {}
+        r['Trip ID'] = result[0]
+        r['Start Time'] = result[1]
+        r['Distance'] = result[3]
+        r['Duration'] = result[4]
+        r['Start Tract'] = result[6]
+        r['End Tract'] = result[7]
+        r['Start Community #'] = result[8]
+        r['End Community #'] = result[9]
+        r['Start Community Name'] = result[10]
+        r['End Community Name'] = result[11]
+        r['Start Lat'] = float(result[12])
+        r['Start Long'] = float(result[13])
+        r['End Lat'] = float(result[15])
+        r['End Long'] = float(result[16])
+        scootJson.append(r)
+    
+    return jsonify(scootJson)
