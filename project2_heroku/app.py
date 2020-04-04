d4c97e663a94b79d14554a166469a0be372b0ebc
michaelpkuhn <rakuhn14@gmail.com>
2020-04-04 12:53:26 -0500

    heroku db except run

--- project2_heroku/app.py
+++ project2_heroku/app.py
@@ -40,28 +40,37 @@ engine = create_engine(db_url)
 
 @app.route("/api/scoot")
 def scoot():
-
-    scootResults = engine.execute("Select * from scoot WHERE start_lat is not null LIMIT 10000").fetchall()
-    scootJson = []
-    #Start and End Time rounded to the nearest hour
-    for result in scootResults:
-        r = {}
-        r['Trip ID'] = result[0]
-        r['Start Time'] = result[1]
-        r['Distance'] = result[3]
-        r['Duration'] = result[4]
-        r['Start Tract'] = result[6]
-        r['End Tract'] = result[7]
-        r['Start Community #'] = result[8]
-        r['End Community #'] = result[9]
-        r['Start Community Name'] = result[10]
-        r['End Community Name'] = result[11]
-        r['Start Lat'] = float(result[12])
-        r['Start Long'] = float(result[13])
-        r['End Lat'] = float(result[15])
-        r['End Long'] = float(result[16])
-        scootJson.append(r)
-    return jsonify(scootJson)
+    def createScoot(scootResults):
+        scootJson = []
+        #Start and End Time rounded to the nearest hour
+        for result in scootResults:
+            r = {}
+            r['Trip ID'] = result[0]
+            r['Start Time'] = result[1]
+            r['Distance'] = result[3]
+            r['Duration'] = result[4]
+            r['Start Tract'] = result[6]
+            r['End Tract'] = result[7]
+            r['Start Community #'] = result[8]
+            r['End Community #'] = result[9]
+            r['Start Community Name'] = result[10]
+            r['End Community Name'] = result[11]
+            r['Start Lat'] = float(result[12])
+            r['Start Long'] = float(result[13])
+            r['End Lat'] = float(result[15])
+            r['End Long'] = float(result[16])
+            scootJson.append(r)
+        return scootJson
+        
+    try:
+        scootResults = engine.execute("Select * from scoot WHERE start_lat is not null LIMIT 10000").fetchall()
+        scootJson = createScoot(scootResults)
+        return jsonify(scootJson)
+    except:
+        engine = create_engine(db)
+        scootResults = engine.execute("Select * from scoot WHERE start_lat is not null LIMIT 10000").fetchall()
+        scootJson = createScoot(scootResults)
+        return jsonify(scootJson)
 
 @app.route("/api/divvy")
 def divvy():
